dd6beac0d3dd25c451ff7c5fb13737bc37876907
zabackary <137591653+zabackary@users.noreply.github.com>
2024-03-27 13:10:28 +0900

    make TS happy

--- src/game/levels/fall.ts
+++ src/game/levels/fall.ts
@@ -1,19 +1,12 @@
-import { Match } from "../../framework/components/conditionals";
 import { PIXEL_ART_SIZE } from "../gameRoot";
 import labeledImageButton from "../utils/labeledImageButton";
 import outsideScene from "./fall/outsideScene";
 
 export default function fall(onComplete: () => void) {
-  let sceneSwitcher: Match;
   return [
-    (sceneSwitcher = new Match(
-      {
-        outsideScene: outsideScene(() => {
-          onComplete();
-        }),
-      },
-      "outsideScene"
-    )),
+    outsideScene(() => {
+      onComplete();
+    }),
     labeledImageButton(
       "assets/shared/home.png",
       "assets/shared/home-pressed.png",
